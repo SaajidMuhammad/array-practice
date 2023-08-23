080ca8b9a4d0b112ef1789074c053bc87175ed52
saajidmhd <muhammad@hatchyard.io>
2023-08-23 21:30:32 +0530

    feat: [XY-48] login feature

---
+++ index.js
@@ -0,0 +1,24 @@
+let number1 = 100; // number
+let number2 = 200; // number
+let birthYear = 1990
+
+// string
+let firstName = `My Name is Saajid, My Age is ${2023 - 1990}`;
+
+// "string "
+// 'string'
+// ``
+
+//  0 1 2
+
+console.log(firstName);
+
+// number
+
+// methods && functions
+
+// user create functions
+// inbuild --->
+
+// + --> add
+// + --> string + string
